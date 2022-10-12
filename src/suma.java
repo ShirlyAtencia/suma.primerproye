58789f922463cf08086122555e5af2a08b662b12
ShirlyAtencia <ing.atenciashirly@gmail.com>
2022-10-12 11:22:08 -0500

    Reto Suma

---
+++ src/suma.java
@@ -0,0 +1,12 @@
+public class suma {
+  public static void main(String[] args) {
+    int resultado = 0;
+    resultado = sumas(10, 20, 30);
+    System.out.println(resultado);
+  }
+
+  public static int sumas(int a, int b, int c) {
+    return a + b + c;
+
+  }
+}
